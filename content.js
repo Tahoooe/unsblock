c3e20c7696f8d8b58f8ab0566aaa9e111931c92a
Tahoe <bonjour@tahoe.be>
2021-05-18 14:56:40 +0200

    Échec

--- content.js
+++ content.js
@@ -1,26 +1,27 @@
-const toBlock = ['ralphkayden', 'elise_outside'];
-
-
 let allPhotos;
 function refresh() {
-  allPhotos = document.querySelectorAll('figure');
-  console.log(allPhotos);
+  allPhotos = document.querySelectorAll('figure:not(.verified');
+  // console.log(allPhotos);
 }
-  
-
-
 
 function removePhotos() {
       refresh();
-  
+      updateTargets();
+      // console.log(window.location.href);
   
   allPhotos.forEach(function(photo) {
-    let photoAuthor = photo.querySelector('._1FfS9').href.toString().replace('https://unsplash.com/@', '');
-    
+    let photoAuthor = photo.querySelector('._1FfS9');
+    if (photoAuthor) {
+      photoAuthor = photoAuthor.href.toString().replace('https://unsplash.com/@', '')
+    }
 
-      console.log(photoAuthor);
+
     
-    if (photoAuthor === 'elise_outside') { photo.remove(); }
+    if (photoAuthor === 'dollargill') {
+      photo.remove();
+    } else {
+      photo.classList.add('verified');
+    }
       
     
     
@@ -28,13 +29,49 @@ function removePhotos() {
   });
 }
 
-if( document.readyState !== 'loading' ) {
-    console.log( 'document is already ready, just execute code here' );
-    removePhotos();
+window.addEventListener("load", removePhotos);
 
-} else {
-    document.addEventListener('DOMContentLoaded', function () {
-        console.log( 'document was not ready, place code here' );
-        removePhotos();
-    });
+
+// je veux que ça change quand
+// - on ouvre une photo (target body)
+// - on passe à une autre photo 
+
+
+var config = { attributes: true, childList: true, characterData: true };
+var observer = new MutationObserver(function(mutations) {
+  mutations.forEach(function(mutation) {
+    // removePhotos();
+
+    // quand <head> change
+    if (mutation.target === document.head) {
+      console.log("changement de head");
+
+      updateTargets();
+
+      
+    } else {
+      console.log('autre changement');
+      removePhotos();
+    }
+
+  });
+});
+
+function updateTargets() {
+  let newTargets = document.querySelectorAll('._1ZjfQ');
+
+  newTargets.forEach(function(newTarget) {
+    observer.observe(newTarget, config);
+  });
 }
+
+
+
+// observer.observe(document.querySelector('._1ZjfQ'), config);
+observer.observe(document.head, config);
+
+// otherwise
+observer.disconnect();
+// observer.observe(document.querySelector('._1ZjfQ'), config);
+observer.observe(document.head, config);
+
